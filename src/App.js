f067036e1fdb75c6c95ea7d35f2f81dadc04f8bf
Tien Web <tienzs280@gmail.com>
2022-01-13 10:46:23 +0700

    add file nha

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-/*import logo from './logo.svg';*/
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
-import { Component } from 'react';
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      dishes: DISHES
-    };
-  }
   render () {
     return (
-      <div className="App">
-        <Navbar dark color='primary'>
-          <div className='container'>
-            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+      <div>
+        <Main />
       </div>
     );
   }
