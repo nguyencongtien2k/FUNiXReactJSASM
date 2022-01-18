8c8712e735c75ffef74830e416877e30813a0d59
Tien Web <tienzs280@gmail.com>
2022-01-18 09:39:01 +0700

    change file

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -50,9 +50,9 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
-          <Route exact path="/aboutus" component={About} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
