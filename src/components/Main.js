21707267c33196f8da1cec591fadedb2339f521e
Tien Web <tienzs280@gmail.com>
2022-02-13 19:40:50 +0700

    Intro. Redux

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,11 +16,6 @@ const mapStateToProps = state => {
 }
 
 class Main extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const StaffWithId = ({match}) => {
             return(
